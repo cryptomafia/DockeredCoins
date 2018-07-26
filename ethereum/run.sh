50faebab446650bc7ec467c5f87f25f448d531eb
vassilyvv <vassily.v.v@gmail.com>
2018-07-26 17:59:45 +0400

    0.0.0.0 for geth

--- ethereum/run.sh
+++ ethereum/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "$TESTNET" -eq 0 ]] ; then
    echo Starting ethereum main network
-   geth --syncmode --rpcaddr "0.0.0.0" --rpccorsdomain "geth" --rpcport "8545" && sleep 5d
+   geth --syncmode --rpcaddr "0.0.0.0" --rpccorsdomain "geth" --rpcport "8545" --rpc && sleep 5d
 else
    echo Starting ethereum test network
-   geth --syncmode --testnet  --rpcaddr "0.0.0.0" --rpccorsdomain "geth" --rpcport "8545" && sleep 5d
+   geth --syncmode --testnet  --rpcaddr "0.0.0.0" --rpccorsdomain "geth" --rpcport "8545" --rpc && sleep 5d
 fi
