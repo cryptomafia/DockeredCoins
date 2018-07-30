2bc791f34b3fbfa1e562195dce71109bcfa58b06
vassilyvv <vassily.v.v@gmail.com>
2018-07-30 16:17:02 +0400

    geth testnet???

--- ethereum/run.sh
+++ ethereum/run.sh
@@ -3,5 +3,5 @@ if [[ "$TESTNET" -eq 0 ]] ; then
    echo Starting ethereum main network
 else
    echo Starting ethereum test network
-   geth --rinkeby --networkid 4 --rpc --rpcaddr 0.0.0.0 --rpcapi 'admin,debug,shh,txpool,personal,eth,net,web3' --syncmode 'full' && sleep 5d
+   geth --rinkeby --networkid 4 --rpc --rpcaddr 127.0.0.1 --rpcapi 'admin,debug,miner,shh,txpool,personal,eth,net,web3'  --syncmode 'light' console
 fi
