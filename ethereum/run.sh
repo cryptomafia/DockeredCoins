1a7b97f157201c393eff51a0826bdd030b50edae
vassilyvv <vassily.v.v@gmail.com>
2018-07-30 15:36:19 +0400

    geth testnet??

--- ethereum/run.sh
+++ ethereum/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [[ "$TESTNET" -eq 0 ]] ; then
    echo Starting ethereum main network
-   geth --syncmode="full" --rpcaddr "0.0.0.0" --rpccorsdomain "geth" --rpc && sleep 5d
 else
    echo Starting ethereum test network
-   geth --testnet --syncmode="full" --rpcaddr "0.0.0.0" --rpccorsdomain "geth" --rpc && sleep 5d
+   geth --rpcapi personal,eth,net,web3 --rpcaddr 0.0.0.0 --rpc --testnet && sleep 5d
 fi
