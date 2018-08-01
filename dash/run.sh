0a1df1365b41748d61889796dd60d4bcfbc720c8
vassilyvv <vassily.v.v@gmail.com>
2018-08-01 17:36:12 +0400

    dash dirs fix

--- dash/run.sh
+++ dash/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "TESTNET" -eq 0 ]] ; then
    echo Starting dash main network
-   dashd --conf=/root/.dash/dash.conf && sleep 5d
+   dashd -datadir=/root/.dash --conf=/root/.dash/dash.conf && sleep 5d
 else
    echo Starting dash test network
-   dashd -testnet --conf=/root/.dash/dash.conf --datadir=/root/.dash && sleep 5d
+   dashd -testnet -datadir=/root/.dash --conf=/root/.dash/dash.conf && sleep 5d
 fi
